4ebe1320ec5a7d394d322fe5b2709850e1478d89
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-09-15 09:02:13 +0200

    Evaluator implemented

---
+++ DiGi.Typology/Classes/TypologySolver.cs
@@ -0,0 +1,15 @@
+﻿using DiGi.Core.Interfaces;
+using DiGi.Typology.Interfaces;
+
+namespace DiGi.Typology.Classes
+{
+    public class TypologySolver : ITypologyObject, ISolver
+    {
+        private readonly Typology? typology;
+
+        public bool Solve()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
